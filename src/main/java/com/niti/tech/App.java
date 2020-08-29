8d3a7d806752710b623ed810475d93005e1f5e45
suresh <sureshc07771@gmail.com>
2020-08-29 18:53:59 +0530

    message

---
+++ src/main/java/com/niti/tech/App.java
@@ -0,0 +1,13 @@
+package com.niti.tech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
